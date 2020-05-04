1097050445dd165fe0c399c877d3b74cb4ad603a
Vixtor61 <00198117@uca.edu.sv>
2020-05-03 20:30:01 -0600

    Create Project

---
+++ actividadL3/src/main/java/com/uca/actividadL3/ActividadL3Application.java
@@ -0,0 +1,13 @@
+package com.uca.actividadL3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadL3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadL3Application.class, args);
+	}
+
+}
---
+++ actividadL3/src/test/java/com/uca/actividadL3/ActividadL3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.actividadL3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadL3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
